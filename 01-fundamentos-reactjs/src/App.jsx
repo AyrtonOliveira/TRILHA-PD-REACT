64dc93e11acfc9c4c71bdf144a7e9d86736837de
Ayrton Oliveira <ayrtonjose_2011@hotmail.com>
2025-04-25 15:46:23 -0300

    alteração nome post 2
    Alteração nome post 2

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -7,7 +7,7 @@ export function App() {
         author="Ayrton Oliveira"
         content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat quo maiores pariatur, est beatae enim, quibusdam iusto, accusamus numquam tempora cum eius facere quod expedita aperiam aut ab ipsa laborum?"
       />
-      <Post author="Cleide Oliveira" content="Um post muito legal!" />
+      <Post author="Cleide Maria" content="Um post muito legal!" />
     </div>
   );
 }
